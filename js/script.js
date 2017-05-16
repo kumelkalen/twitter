0995d20dcf98ef1f479e5012d8c225f126a2fb1c
kumelkalen <free.your.mind.fc@gmail.com>
2017-05-16 12:06:46 -0400

    Llamamos al contenedor y además validamos que el usuario ingrese una tarea antes de que cree el nuevo texto

--- js/script.js
+++ js/script.js
@@ -2,7 +2,20 @@ function agregar(){
 	//Limpio el texto ingresado en tarea
 	var tareas = document.getElementById("tarea").value;
 	document.getElementById("tarea").value = "";
-	
+		
+	//Llamo al contenedor
+	var cont = document.getElementById("contenedor");
+
+	//Validar que no se ingrese una tarea vacía
+	if(tareas==null||tareas.length==0){
+		alert("¡Error! Debe ingresar tarea");
+		return false;
+	}
+
+	//Nuevos nodos elementos (div,span y creamos un text node tarea)
+	var nuevasTareas = document.createElement("div");
+	var elementoContenedor = document.createElement("span"); 
+	var textoNuevaTarea = document.createTextNode(tareas);
 
 }
 
